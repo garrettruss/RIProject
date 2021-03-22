63c2c96cf51cdb60631a12e6087bdd8d73e769d4
garrettruss <garrettmruss@gmail.com>
2021-03-22 12:43:23 -0400

    dotenv update

--- config/database.js
+++ config/database.js
@@ -1,27 +1,4 @@
 
-/* v2 */
-
-// Require Database
-const mongoose = require('mongoose');
-
-//Database Connection
-mongoose.connect('mongodb+srv://GarrettRuss:Riley2021@cluster0.d4qrc.mongodb.net/access?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-});
-
-// shortcut to mongoose.connection object
-const db = mongoose.connection;
-
-//Check connection
-db.on('connected', function () {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
-});
-
-/*
-v3
 mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -39,5 +16,3 @@ const db = mongoose.connection;
 db.on('connected', function () {
     console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
-
-*/
