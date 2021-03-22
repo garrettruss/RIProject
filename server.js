ddd3b5c9130d41ea14c1c5ecc91f4d4f19fe1f3e
garrettruss <garrettmruss@gmail.com>
2021-03-22 13:52:35 -0400

    fixs: adds env var access for port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
