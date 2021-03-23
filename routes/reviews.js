0d66d72a0d92f0af8c499dacc1601989f9802cbf
garrettruss <garrettmruss@gmail.com>
2021-03-23 13:24:45 -0400

    redo

--- routes/reviews.js
+++
@@ -1,9 +0,0 @@
-/*
-const router = require('express').Router();
-
-const reviewsCtrl = require('../controllers/reviews');
-
-router.post('/rows/:id/reviews', reviewsCtrl.create);
-
-module.exports = router;
-*/
